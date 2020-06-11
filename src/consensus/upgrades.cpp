65231f64ca8b2932f10a46b1cc1f940dd9e14d1b
Kris Nuttycombe <kris@electriccoin.co>
2020-06-11 10:25:05 -0600

    Add "NEXT" consensus branch ID.

--- src/consensus/upgrades.cpp
+++ src/consensus/upgrades.cpp
@@ -43,6 +43,11 @@ const struct NUInfo NetworkUpgradeInfo[Consensus::MAX_NETWORK_UPGRADES] = {
         /*.nBranchId =*/ 0xe9ff75a6,
         /*.strName =*/ "Canopy",
         /*.strInfo =*/ "See https://z.cash/upgrade/canopy/ for details.",
+    },
+    {
+        /*.nBranchId =*/ 0x7374f403,
+        /*.strName =*/ "NEXT",
+        /*.strInfo =*/ "See https://z.cash/upgrade/NEXT/ for details.",
     }
 };
 
